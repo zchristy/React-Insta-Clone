67d6c59489ba06656ca6bccd349bd4f42b5044f4
zchristy <zchristy44@gmail.com>
2019-04-28 23:30:03 -0500

    Basic structure complete

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import CommentSection from './components/CommentSection/CommentSection';
+
+import dummyData from './dummy-data';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = dummyData;
+  }
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <PostContainer />
+        <CommentSection />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="SearchBar">
+
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
